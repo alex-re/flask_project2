d3c5183a142e2e2ce2911d08869601d4041416f1
alex-from-ubuntu <chalghoos1@gmail.com>
2020-08-30 17:05:43 +0430

    session 2 templates

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,18 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
 
 
+posts = [
+    {
+        'author': 'ali',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'gholi',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 20, 2020'
+    }
+]
+
+
 @app.route("/")
 @app.route("/home")
-def hello():
-    return "<h1>Home Page</h1>"
+def home():
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 

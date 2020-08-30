8ff311f32fe7dacc17906c6f9519f19687b983ce
alex-from-ubuntu <chalghoos1@gmail.com>
2020-08-31 00:51:40 +0430

    session 3 login and register WITHOUT db

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 
 app = Flask(__name__)
 
+# import secrets; secret.token_hex(16)  # argiuments (16) is optional.
+app.config["SECRET_KEY"] = '8e6880f71e7fb8ad8e502554b1e8a244'
 
 posts = [
     {
@@ -29,6 +32,27 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Accont created for {form.username.data} !', 'success')
+        return redirect(url_for('home'))  # note: `url_for` gets the FUNCTION NAME
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'test@test.com' and form.password.data == 'abc123':
+            flash("You have been loged in!", "success")
+            return redirect(url_for('home'))
+        else:
+            flash("Login Unsuccessful. Please check username and password", 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 

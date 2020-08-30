cdaff8d1987b7cb9ab55077b8f268dee3d4991fc
alex-from-ubuntu <chalghoos1@gmail.com>
2020-08-31 01:55:25 +0430

    db start

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,12 +1,41 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 app = Flask(__name__)
 
 # import secrets; secret.token_hex(16)  # argiuments (16) is optional.
 app.config["SECRET_KEY"] = '8e6880f71e7fb8ad8e502554b1e8a244'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db'
 
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(30), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.png')
+    password = db.Column(db.String(60), nullable=False)  # we will hash passwords
+    posts = db.relationship('Post', backref='author', lazy=True)  # upper case because it hints to a "class"
+
+    def __repr__(self):  # like __str__
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # lower case because it hints to a "table name and a column name"
+
+    def __repr__(self):  # like __str__
+        return f"Post('{self.title}', '{self.date_posted}')"
+#  from flaskblog import db  # (with a lot output :)
+#  db.create_all()  # db.drop_all()
 posts = [
     {
         'author': 'ali',

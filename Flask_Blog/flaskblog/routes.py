0bbc9a95c316265820a4291cc6e968ebca415780
alex-from-ubuntu <chalghoos1@gmail.com>
2020-09-02 16:24:00 +0430

    pagination user specific page

--- Flask_Blog/flaskblog/routes.py
+++ Flask_Blog/flaskblog/routes.py
@@ -17,7 +17,9 @@ from PIL import Image
 @app.route("/")
 @app.route("/home")
 def home():
-    posts = Post.query.all()
+    # posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)  # default value is 1
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page=5, page=page)  # order_by for reversing.
     return render_template('home.html', posts=posts)
 
 
@@ -165,3 +167,15 @@ def delete_post(post_id):
         db.session.commit()
         flash('Your post has been deleted!', 'success')
         return redirect(url_for('home'))
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()    
+    
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(per_page=5, page=page)
+    
+    return render_template('user_posts.html', posts=posts, user=user, title=user.username)
